507115c99e9720dc966994db42c8261144aec364
Leonardo Dias <contato.leonardod@yahoo.com>
2020-01-25 18:42:27 -0300

    add pagination and refactiong Hipsters scraper

--- server/src/services/hipsters.js
+++ server/src/services/hipsters.js
@@ -1,76 +1,108 @@
 import cheerio from 'cheerio';
 import fetchHTML from './request';
 
-const parseData = html => {
-  let jobs = [];
+const scrapData = async () => {
+  const $ = await getPageSelector();
 
-  try {
-    const $ = cheerio.load(html);
+  const firstPageJobs = extractJobs($);
 
-    const articles = $('body')
-      .find("div[class='container']")
-      .find('.search-results > article');
+  const jobsPerPage = firstPageJobs.length;
 
-    articles.each((index, element) => {
-      const location = $(element)
-        .find(
-          "span[class='listing-item__info--item listing-item__info--item-location']"
-        )
-        .text()
-        .trim();
+  const pagesCount = getPagesCount($, jobsPerPage);
 
-      const company = $(element)
-        .find(
-          "span[class='listing-item__info--item listing-item__info--item-company']"
-        )
-        .text()
-        .trim();
+  const otherPagesJobs = await getAllJobs(2, pagesCount);
 
-      const employmentType = $(element)
-        .find("span[class='listing-item__employment-type']")
-        .text()
-        .trim();
+  const jobs = [...firstPageJobs, ...otherPagesJobs];
 
-      const title = $(element)
-        .find("a[class='link']")
-        .text()
-        .trim();
-
-      const date = $(element)
-        .find("div[class='listing-item__date']")
-        .text()
-        .trim();
-
-      const link = $(element)
-        .find("a[class='link']")
-        .attr('href')
-        .trim();
-
-      const job = {
-        title,
-        company,
-        location,
-        date,
-        employmentType,
-        link,
-      };
-
-      jobs.push(job);
-    });
-
-    return jobs;
-  } catch (error) {
-    console.log('error on PARSE');
-
-    console.log(error);
-  }
+  return jobs;
 };
 
-const scrapData = async () => {
-  const responseHtml = await fetchHTML(
-    'https://hipsters.jobs/jobs/?q=&l=Remoto'
+const getAllJobs = async (pageStart = 1, pagesCount) => {
+  let jobs = [];
+  for (let page = pageStart; page <= pagesCount; page++) {
+    const pageSelector = await getPageSelector(page);
+    const pageJobs = extractJobs(pageSelector);
+
+    jobs = [...jobs, ...pageJobs];
+  }
+
+  return jobs;
+};
+
+const getPageSelector = async (page = 1) => {
+  const html = await fetchHTML(
+    `https://hipsters.jobs/jobs/?city=Remote&page=${page}`
   );
-  const jobs = parseData(responseHtml);
+
+  const $ = cheerio.load(html);
+
+  return $;
+};
+
+const getPagesCount = ($, perPageAmount) => {
+  const amountText = $('h1.search-results__title')
+    .text()
+    .trim();
+
+  const totalAmount = amountText.match(/\d+\s/)[0];
+  const pagesCount = Math.ceil(totalAmount / perPageAmount);
+
+  return pagesCount;
+};
+
+const extractJobs = $ => {
+  let jobs = [];
+
+  const articles = $('body')
+    .find("div[class='container']")
+    .find('.search-results > article');
+
+  articles.each((index, element) => {
+    const location = $(element)
+      .find(
+        "span[class='listing-item__info--item listing-item__info--item-location']"
+      )
+      .text()
+      .trim();
+
+    const company = $(element)
+      .find(
+        "span[class='listing-item__info--item listing-item__info--item-company']"
+      )
+      .text()
+      .trim();
+
+    const employmentType = $(element)
+      .find("span[class='listing-item__employment-type']")
+      .text()
+      .trim();
+
+    const title = $(element)
+      .find("a[class='link']")
+      .text()
+      .trim();
+
+    const date = $(element)
+      .find("div[class='listing-item__date']")
+      .text()
+      .trim();
+
+    const link = $(element)
+      .find("a[class='link']")
+      .attr('href')
+      .trim();
+
+    const job = {
+      title,
+      company,
+      location,
+      date,
+      employmentType,
+      link,
+    };
+
+    jobs.push(job);
+  });
 
   return jobs;
 };
