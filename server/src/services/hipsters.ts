34af76b5f204ccc2860187ed2cf43fe455ef16c3
Leonardo Dias <coelholeonardodias@gmail.com>
2020-01-28 12:23:13 -0300

    migrate Hipsters to typescript

--- server/src/services/hipsters.ts
+++ server/src/services/hipsters.ts
@@ -1,5 +1,6 @@
 import cheerio from 'cheerio';
 import fetchHTML from './request';
+import Job from '../job/job';
 
 const scrapData = async () => {
   const $ = await getPageSelector();
@@ -17,8 +18,8 @@ const scrapData = async () => {
   return jobs;
 };
 
-const getAllJobs = async (pageStart = 1, pagesCount) => {
-  let jobs = [];
+const getAllJobs = async (pageStart = 1, pagesCount: number) => {
+  let jobs = Array<Job>();
   for (let page = pageStart; page <= pagesCount; page++) {
     const pageSelector = await getPageSelector(page);
     const pageJobs = extractJobs(pageSelector);
@@ -29,7 +30,7 @@ const getAllJobs = async (pageStart = 1, pagesCount) => {
   return jobs;
 };
 
-const getPageSelector = async (page = 1) => {
+const getPageSelector = async (page = 1): Promise<CheerioStatic> => {
   const html = await fetchHTML(
     `https://hipsters.jobs/jobs/?city=Remote&page=${page}`
   );
@@ -39,19 +40,19 @@ const getPageSelector = async (page = 1) => {
   return $;
 };
 
-const getPagesCount = ($, perPageAmount) => {
+const getPagesCount = ($: CheerioStatic, perPageAmount: number): number => {
   const amountText = $('h1.search-results__title')
     .text()
     .trim();
 
-  const totalAmount = amountText.match(/\d+\s/)[0];
+  const totalAmount = parseInt(amountText.match(/\d+\s/)[0]);
   const pagesCount = Math.ceil(totalAmount / perPageAmount);
 
   return pagesCount;
 };
 
-const extractJobs = $ => {
-  let jobs = [];
+const extractJobs = ($: CheerioStatic) => {
+  const jobs = Array<Job>();
 
   const articles = $('body')
     .find("div[class='container']")
@@ -92,7 +93,7 @@ const extractJobs = $ => {
       .attr('href')
       .trim();
 
-    const job = {
+    const job: Job = {
       title,
       company,
       location,
