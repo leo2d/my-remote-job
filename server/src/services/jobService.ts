8f5de728bb471a9cbef5bc2a6b8a3773804c6def
Leonardo Dias <contato.leonardod@yahoo.com>
2020-02-03 21:39:22 -0300

    fix query on find job

--- server/src/services/jobService.ts
+++ server/src/services/jobService.ts
@@ -2,8 +2,9 @@ import Job from '../models/job';
 
 const getByOrigin = async (origin: string) => {
     try {
-        const jobs = await Job.find((err, resp) => {
-            resp.find(x => x.origin === origin);
+        const jobs = await Job.find({ origin }, (err, res) => {
+            if (err) console.log(`ERROR : ${err}`);
+            return res;
         });
 
         return jobs;
