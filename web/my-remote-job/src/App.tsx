2aaf19af92b74ee558a92c8814599390757b6317
Leonardo Dias <contato.leonardod@yahoo.com>
2020-05-23 19:10:14 -0300

    add helmet to maage title and head

--- web/my-remote-job/src/App.tsx
+++ web/my-remote-job/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Home from './pages/Home';
 import { GlobalStyle } from './styles/global-style';
+import { Helmet } from 'react-helmet';
 
 const App: React.FC = () => {
     return (
         <GlobalStyle>
             <div className="App">
-                <Home/>
+                <Helmet>
+                    <title>My Remote Job</title>
+                </Helmet>
+                <Home />
             </div>
         </GlobalStyle>
     );
