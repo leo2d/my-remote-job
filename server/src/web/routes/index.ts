198130a70757676b8c89589b702ebaffab50150b
Leonardo Dias <contato.leonardod@yahoo.com>
2020-05-29 19:48:04 -0300

    add health check

--- server/src/web/routes/index.ts
+++ server/src/web/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 import { scraperRoutes } from './scraper';
 import { jobsRoutes } from './jobs';
+import healthCheckRoutes from './healthCheckRoutes';
 
 const routes: Router = Router();
 
+routes.use('/health', healthCheckRoutes);
 routes.use('/scraper', scraperRoutes);
 routes.use('/jobs', jobsRoutes);
 
