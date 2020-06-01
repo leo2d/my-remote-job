f7a7b0f0163ddec01e9396f287b38f4c1c4dcd37
Leonardo Dias <contato.leonardod@yahoo.com>
2020-06-01 19:00:53 -0300

    add a start function

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,22 +6,26 @@ import swaggerUi from 'swagger-ui-express';
 import * as swaggerDoc from './swaggerDoc.json';
 import { SERVER_PORT } from './config/config';
 
-const app = express();
-app.use(express.json());
+const start = () => {
+    const app = express();
+    app.use(express.json());
 
-var whitelist = ['http://localhost:3000', 'http://127.0.0.1:3000'];
-var corsOptions = {
-    origin: function(origin: any, callback: any) {
-        if (whitelist.indexOf(origin) !== -1) callback(null, true);
-        else callback(new Error('Not allowed by CORS'));
-    },
+    var whitelist = ['http://localhost:3000', 'http://127.0.0.1:3000'];
+    var corsOptions = {
+        origin: function(origin: any, callback: any) {
+            if (whitelist.indexOf(origin) !== -1) callback(null, true);
+            else callback(new Error('Not allowed by CORS'));
+        },
+    };
+
+    app.use(cors(corsOptions));
+    app.use('/api', routes);
+
+    app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+    app.listen(SERVER_PORT, () =>
+        console.log(`Scraper listening on port ${SERVER_PORT}!`)
+    );
 };
 
-app.use(cors(corsOptions));
-app.use('/api', routes);
-
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
-app.listen(SERVER_PORT, () =>
-    console.log(`Scraper listening on port ${SERVER_PORT}!`)
-);
+start();
