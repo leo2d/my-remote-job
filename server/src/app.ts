7d26355d90dedd57798ad46283dd2514d6314579
Leonardo Dias <contato.leonardod@yahoo.com>
2020-07-05 22:43:13 -0300

    fix: change cors policy

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,15 +8,8 @@ const start = () => {
     const app = express();
     app.use(express.json());
 
-    var whitelist = ['http://localhost:3000', 'http://127.0.0.1:3000'];
-    var corsOptions = {
-        origin: function(origin: any, callback: any) {
-            if (whitelist.indexOf(origin) !== -1) callback(null, true);
-            else callback(new Error('Not allowed by CORS'));
-        },
-    };
+    app.use(cors());
 
-    app.use(cors(corsOptions));
     app.use('/api', routes);
 
     app.listen(SERVER_PORT, () =>
