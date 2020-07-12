c229bba70ca7b9d63d28f57ac779274ac945201c
Leonardo Dias <contato.leonardod@yahoo.com>
2020-07-12 18:05:38 -0300

    refactor: change metas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,7 +8,9 @@ const App: React.FC = () => {
         <GlobalStyle>
             <div className="App">
                 <Helmet>
+                    <meta charSet="utf-8" />
                     <title>My Remote Job</title>
+                    <meta name="description" content="Find your next remote job today!" />
                 </Helmet>
                 <Home />
             </div>
